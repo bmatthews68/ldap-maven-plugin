795fc41e1692220f0aaa8a5b22d474d50089deaa
Brian Matthews <brian@btmatthews.com>
2012-12-20 16:08:57 +0000

    Added favicon

---
+++ src/main/java/com/btmatthews/maven/plugins/ldap/opendj/OpenDJServer.java
@@ -0,0 +1,11 @@
+package com.btmatthews.maven.plugins.ldap.opendj;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Brian
+ * Date: 17/12/12
+ * Time: 04:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class OpenDJServer {
+}
